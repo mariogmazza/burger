1f85e6f26c00ccc69495fb0ebafc349d4770e04a
Mario Mazza <mariomazza01@gmail.com>
2017-10-05 22:31:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "tucuman254",
+  password: "",
   database: "burgerK_db"
 });
 
